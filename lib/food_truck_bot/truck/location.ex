f56c4655a05ebb137ac1fb2490d5949ba28f2242
Jake Morrison <jake@cogini.com>
2023-05-11 15:44:19 -0500

    Define Truck context and Location

---
+++ lib/food_truck_bot/truck/location.ex
@@ -0,0 +1,54 @@
+defmodule FoodTruckBot.Truck.Location do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "locations" do
+    field :address, :string
+    field :applicant, :string
+    field :approved, :utc_datetime
+    field :days, :string
+    field :expiration_date, :utc_datetime
+    field :facliity_type, :string
+    field :food_items, :string
+    field :latitude, :float
+    field :location_description, :string
+    field :longitude, :float
+    field :permit, :string
+    field :status, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(location, attrs) do
+    location
+    |> cast(attrs, [
+      :applicant,
+      :facliity_type,
+      :location_description,
+      :address,
+      :permit,
+      :status,
+      :food_items,
+      :latitude,
+      :longitude,
+      :days,
+      :approved,
+      :expiration_date
+    ])
+    |> validate_required([
+      # :applicant,
+      # :facliity_type,
+      :location_description,
+      :address,
+      # :permit,
+      :status,
+      :food_items,
+      # :latitude,
+      # :longitude,
+      # :days,
+      # :approved,
+      # :expiration_date
+    ])
+  end
+end
