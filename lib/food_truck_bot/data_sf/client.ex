105ea6bd4a3d2b5ceadd6bfb3c724f3af6e89db1
Jake Morrison <jake@cogini.com>
2023-05-11 15:43:54 -0500

    Fetch and process location data from server

---
+++ lib/food_truck_bot/data_sf/client.ex
@@ -0,0 +1,36 @@
+defmodule FoodTruckBot.DataSF.Client do
+  @moduledoc """
+  Client to fetch data from https://data.sfgov.org/
+  """
+
+  @base_url Application.compile_env(:food_truck_bot, :data_sf_url)
+
+  require Logger
+
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, @base_url
+
+  @spec get_mobile_food_facility_permits() :: {:ok, list(map())} | {:error, binary() | Tesla.Env.t()}
+  def get_mobile_food_facility_permits() do
+
+    case get("/api/views/rqzj-sfat/rows.csv") do
+      {:ok, %{status: 200, body: ""}} ->
+        {:error, "no data"}
+      
+      {:ok, %{status: 200, body: body}} ->
+        {:ok, data} = FoodTruckBot.DataSF.MobileFoodFacilityPermit.parse_csv(body)
+
+        {:ok, data}
+
+      {:ok, result} ->
+        Logger.error("#{inspect(result)}")
+        {:error, result}
+
+      {:error, reason} = result ->
+        Logger.error("#{inspect(reason)}")
+        result
+    end
+  end
+
+end
---
+++ lib/food_truck_bot/data_sf/mobile_food_facility_permit.ex
@@ -0,0 +1,95 @@
+defmodule FoodTruckBot.DataSF.MobileFoodFacilityPermit do
+  @moduledoc """
+  """
+
+  @spec parse_csv(binary()) :: {:ok, list(map())}
+  def parse_csv(csv) do
+    result =
+      csv
+      |> NimbleCSV.RFC4180.parse_string()
+      |> Enum.map(&parse_record/1)
+      |> Enum.map(&filter_empty/1)
+
+    {:ok, result}
+  end
+
+  defp parse_record(
+    [
+      location_id,
+      applicant,
+      facility_type,
+      _cnn,
+      location_description,
+      address,
+      _blocklot,
+      _block,
+      _lot,
+      permit,
+      status,
+      food_items,
+      _x,
+      _y,
+      latitude,
+      longitude,
+      _schedule,
+      dayshours,
+      _noi_sent,
+      approved,
+      _received,
+      _prior_permit,
+      expiration_date,
+      _location,
+      _fire_prevention_districts,
+      _police_districts,
+      _supervisor_districts,
+      _zip_codes,
+      _neighborhoods
+    ]
+  ) do
+  %{
+    id: String.to_integer(location_id),
+    applicant: applicant,
+    facility_type: facility_type,
+    location_description: location_description,
+    address: address,
+    permit: permit,
+    status: status,
+    food_items: food_items,
+    latitude: parse_float(latitude),
+    longitude: parse_float(longitude),
+    dayshours: dayshours,
+    approved: parse_datetime(approved),
+    expiration_date: parse_datetime(expiration_date)
+  }
+  end
+
+  def parse_datetime(""), do: nil
+  def parse_datetime(value) do
+    case Timex.parse(value, "{M}/{D}/{YYYY} {h12}:{m}:{s} {AM}") do
+    {:ok, dt} ->
+      Timex.to_datetime(dt, "America/Los_Angeles")
+    _ ->
+      nil
+    end
+  end
+
+  def parse_float(""), do: nil
+  def parse_float(value) do
+    case Float.parse(value) do
+      {f, _} ->
+        f
+      _ ->
+        nil
+    end
+  end
+
+  # Remove keys from map where value is empty
+  defp filter_empty(m) when is_map(m) do
+    Map.filter(m, &has_value/1)
+  end
+
+  defp has_value({_key, ""}), do: false
+  defp has_value({_key, nil}), do: false
+  defp has_value({_key, _value}), do: true
+
+end
